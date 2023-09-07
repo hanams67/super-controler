349c2fba6d6b1955d2b3221b77893bc141a96fe5
hanams67 <142435256+hanams67@users.noreply.github.com>
2023-09-07 12:23:25 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    radio.sendValue("name", 0)
+})
+function go_forward (bool: boolean, text: string, list: any[], sprite: game.LedSprite, image: Image, num: number) {
+    if (true) {
+        basic.showNumber(randint(Math.round(0), 10))
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff0000)
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showNumber(randint(Math.round(0), 10))
+})
+radio.onReceivedValue(function (name, value) {
+    if (joystick > 10) {
+        cuteBot.moveTime(cuteBot.Direction.forward, 2, 5)
+        joystickbit.Vibration_Motor(100)
+        joystickbit.initJoystickBit()
+    } else if (!((true as any) == (false as any) && ((false as any) == (true as any) || (true == true || false == false)))) {
+    	
+    }
+})
+let joystick = 0
+turtle.forward(1)
+control.inBackground(function () {
+    joystick = joystick
+    joystick += joystick
 })
